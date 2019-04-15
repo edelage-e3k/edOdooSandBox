9764a5a3585e473731b4a11901bd297a2f1788b5
edelage-e3k <edelage@e3k.co>
2019-04-15 10:29:40 -0400

    autre correctif indentation 2

--- livres_module/controllers/controllers.py
+++ livres_module/controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
- class LivresModule(http.Controller):
-     @http.route('/livres_module/livres_module/', auth='public')
-     def index(self, **kw):
-         return "Bonjour les livres"
+class LivresModule(http.Controller):
+    @http.route('/livres_module/livres_module/', auth='public')
+    def index(self, **kw):
+        return "Bonjour les livres"
 
-     @http.route('/livres_module/livres_module/objects/', auth='public')
-     def list(self, **kw):
-         return http.request.render('livres_module.listing', {
-             'root': '/livres_module/livres_module',
-             'objects': http.request.env['livres_module.livres_module'].search([]),
-         })
+    @http.route('/livres_module/livres_module/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('livres_module.listing', {
+            'root': '/livres_module/livres_module',
+            'objects': http.request.env['livres_module.livres_module'].search([]),
+        })
 
-     @http.route('/livres_module/livres_module/objects/<model("livres_module.livres_module"):obj>/', auth='public')
-     def object(self, obj, **kw):
-         return http.request.render('livres_module.object', {
-             'object': obj
-         })
+    @http.route('/livres_module/livres_module/objects/<model("livres_module.livres_module"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('livres_module.object', {
+            'object': obj
+        })
--- livres_module/models/.ipynb_checkpoints/models-checkpoint.py
+++ livres_module/models/.ipynb_checkpoints/models-checkpoint.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
- class livres_module(models.Model):
-     _name = 'livres_module.livres_module'
+class livres_module(models.Model):
+    _name = 'livres_module.livres_module'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-     @api.depends('value')
-     def _value_pc(self):
-         self.value2 = float(self.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
